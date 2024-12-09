ec92210e7375383d6bb3a7c9f0cf6b7f75b788c1
AlexLi727 <alexavililee@gmail.com>
2024-12-09 22:10:51 +0100

    Ultimate

--- 2 - Torre de Hanoi/herrero.js
+++ 2 - Torre de Hanoi/herrero.js
@@ -1,6 +1,8 @@
 let Pila1 = []
 let Pila2 = []
 let Pila3 = []
+
+//Código para empezar la partida, resetendo todos los valores existentes, comprobando el número de piezas introducido y iniciado el array de la pila1
 document.getElementById("empezar").addEventListener("click", function(){
     let nDiscos = parseInt(document.getElementById("discos").value);
     document.getElementById("pila1").innerHTML = "";
@@ -16,6 +18,7 @@ document.getElementById("empezar").addEventListener("click", function(){
     console.log(Pila1[0]);
 });
 
+//Funcion para mostrar de forma visual las pilas, se ejecuta luego de cada actualización
 function updateTower (){
     document.getElementById("pila1").innerHTML = "";
     document.getElementById("pila2").innerHTML = "";
@@ -37,12 +40,15 @@ function updateTower (){
         document.getElementById("pila3").innerHTML += `<div id = pieza`+Pila3[i]+`>`+Pila3[i]+`</div>`;
     }
 
+    //En caso de que la pila1 y pila2 no tengan ninguna pieza pero cuando la pila3 si que contenga, mostrará la pantalla de victoria
     if(Pila1.length === 0 && Pila2.length === 0 && Pila3.length != 0){
         document.getElementById("victoria").innerHTML = "<h1> Enhorabuena! Has movido todas las piezas siguiendo las reglas divinas!!! </h1>";
     }
     
 }
 
+
+//Funciones para mover las piezas a otras pilas, valida si la pieza movida es más pequeño que la base de la pila a excepcion de que no este inicializada
 document.getElementById("p1_btn1").addEventListener("click", function(){
     if(Pila1[0] < Pila2[0] || Pila2.length === 0){
         Pila2.push(Pila1.shift());
--- 3 - Buscaminas/Kovac.js
+++ 3 - Buscaminas/Kovac.js
@@ -1,7 +1,9 @@
-let campoMinas = new Array(8);
-const casilla = new Array(8);
-let nMinas = 10;
-let puntuacion = 0;
+let campoMinas = new Array(8); //Array que usaré que contiene el contenido de cada casilla "minas y numero de minas"
+const casilla = new Array(8); //Array que usaré para mostrar de forma visual el juego. Contiene código HTML
+let nMinas = 10; //Número de minas que tiene el juego
+let puntuacion = 0; //Puntuación de la partida
+
+//Bucle para iniciar el tablero de minas, solo la parte visual y que cada casilla contiene un evento al hacer click.
 for(i = 0; i < campoMinas.length; i++){
     campoMinas[i] = new Array(8);
     casilla[i] = new Array(8);
@@ -22,6 +24,7 @@ for(i = 0; i < campoMinas.length; i++){
     }
 }
 
+//Bucle que pone en casillas aleatoria las minas, verifica que no pueda haber más de 10 minas por fila
 for(i = 0; i < nMinas; i++){
     PosicionMinaX = Math.floor(Math.random() * 8);
     PosicionMinaY = Math.floor(Math.random() * 8);
@@ -33,6 +36,8 @@ for(i = 0; i < nMinas; i++){
     }
 }
 
+/*Bucle que pone los números a cada casilla, se le asigna un número dependiendo del número de minas que tenga esa casilla alrededor y tambien comprueba de que no se salga del límite
+con la función buscarLimite() */
 let minasCasilla = 0;
 for(i = 0; i < campoMinas.length; i++){
     for(y = 0; y < campoMinas[i].length; y++){
@@ -67,27 +72,35 @@ for(i = 0; i < campoMinas.length; i++){
     }
 }
 
+//Funcion que controla lo que pasa en el tablero luego de un evento en concreto
 function prueba(x, y){
-    console.log("xd")
-
+    //Aqui muestro todo el tablero y un mensaje de derrota si se hace clic en una casilla sin mina
     if(campoMinas[x][y] == "M"){
         alert("has perdido");
+        document.getElementById("resultado").innerHTML = "HAS PERDIDO !!!"
         for(i = 0; i < campoMinas.length; i++){
             for(y = 0; y < campoMinas[0].length; y++){
                 casilla[i][y].textContent = campoMinas[i][y];
                 darClase(i, y)
             }
         }
-
+    
+    //Aqui muestro la casilla y los del alrededor que contengan el valor 0 al hacer click sobre una casilla que contenga un 0
     }else if(campoMinas[x][y] == 0){
         vaciar(x, y);
-        
+        casilla[x][y].classList.add("revealed")
+    
+    //Aqui simiplemente muestro la casilla en caso de haber hecho click sobre una que contenga un número que no sea 0, en caso de ser la última casilla acaba la partida
     }else{
         casilla[x][y].textContent = campoMinas[x][y];
         darClase(x, y)
+        if(document.querySelectorAll(".revealed").length == 54){
+            document.getElementById("resultado").innerHTML = "Has ganado !!!";
+        }
     }
 }
 
+//Funcion para buscar y mostrar las casillas que se encuentren alrededor de las casillas con contenido "0"
 function vaciar(x, y){
 
     casilla[x][y].textContent = campoMinas[x][y];
@@ -96,7 +109,7 @@ function vaciar(x, y){
         darClase(x-1, y+1);
     }else if(buscarLimite(x-1, y+1) && campoMinas[x-1][y+1] == 0 && casilla[x-1][y+1].textContent == ""){
         casilla[x-1][y+1].textContent = campoMinas[x-1][y+1];
-        
+        casilla[x-1][y+1].classList.add("revealed")
         vaciar(x-1, y+1);
     }
 
@@ -105,7 +118,7 @@ function vaciar(x, y){
         darClase(x-1, y);
     }else if(buscarLimite(x-1, y) && campoMinas[x-1][y] == 0 && casilla[x-1][y].textContent == ""){
         casilla[x-1][y].textContent = campoMinas[x-1][y];
-        
+        casilla[x-1][y].classList.add("revealed")
         vaciar(x-1, y);
     }
 
@@ -114,7 +127,7 @@ function vaciar(x, y){
         darClase(x-1, y-1);
     }else if(buscarLimite(x-1, y-1) && campoMinas[x-1][y-1] == 0 && casilla[x-1][y-1].textContent == ""){
         casilla[x-1][y-1].textContent = campoMinas[x-1][y-1];
-        
+        casilla[x-1][y-1].classList.add("revealed")
         vaciar(x-1, y-1);
     }
 
@@ -123,7 +136,7 @@ function vaciar(x, y){
         darClase(x, y+1);
     }else if(buscarLimite(x, y+1) && campoMinas[x][y+1] == 0 && casilla[x][y+1].textContent == ""){
         casilla[x][y+1].textContent = campoMinas[x][y+1];
-        
+        casilla[x][y+1].classList.add("revealed")
         vaciar(x, y+1);
     }
 
@@ -132,7 +145,7 @@ function vaciar(x, y){
         darClase(x, y-1);
     }else if(buscarLimite(x, y-1) && campoMinas[x][y-1] == 0 && casilla[x][y-1].textContent == ""){
         casilla[x][y-1].textContent = campoMinas[x][y-1];
-        
+        casilla[x][y-1].classList.add("revealed")
         vaciar(x, y-1);
     }
 
@@ -141,7 +154,7 @@ function vaciar(x, y){
         darClase(x+1, y+1);
     }else if(buscarLimite(x+1, y+1) && campoMinas[x+1][y+1] == 0 && casilla[x+1][y+1].textContent == ""){
         casilla[x+1][y+1].textContent = campoMinas[x+1][y+1];
-        
+        casilla[x+1][y+1].classList.add("revealed")
         vaciar(x+1, y+1);
     }
 
@@ -150,7 +163,7 @@ function vaciar(x, y){
         darClase(x+1, y);
     }else if(buscarLimite(x+1, y) && campoMinas[x+1][y] == 0 && casilla[x+1][y].textContent == ""){
         casilla[x+1][y].textContent = campoMinas[x+1][y];
-        
+        casilla[x+1][y].classList.add("revealed")
         vaciar(x+1, y);
     }
 
@@ -159,18 +172,20 @@ function vaciar(x, y){
         darClase(x+1, y-1);
     }else if(buscarLimite(x+1, y-1) && campoMinas[x+1][y-1] == 0 && casilla[x+1][y-1].textContent == ""){
         casilla[x+1][y-1].textContent = campoMinas[x+1][y-1];
-        
+        casilla[x+1][y-1].classList.add("revealed")
         vaciar(x+1, y-1);
     }
 }
 console.log(campoMinas);
 
-
+//Funcion que devuelve true en caso de que no se trate de un valor que se encuentre fuera de rango de las casillas del tablero de juego
 function buscarLimite(x, y){
     return x >= 0 && x < campoMinas[0].length && y >= 0 && y < campoMinas.length;
 }
 
+//Funcion que otorga dos clases al mostrar su propia casilla.
 function darClase(x, y){
+    casilla[x][y].classList.add("revealed");
     switch(campoMinas[x][y]){
         case 1:
             casilla[x][y].classList.add("n1");
